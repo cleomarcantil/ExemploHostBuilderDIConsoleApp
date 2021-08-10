544e6ef07b19e2569e552aa47cde77258058068f
Cleomar Cantil <cleomarcantil@outlook.com>
2021-08-10 15:42:45 -0300

    Arquivos iniciais do projeto.

---
+++ ExemploHostBuilderDIConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExemploHostBuilderDIConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("ExemploHostBuilderDIConsoleApp");
+
+
+		}
+	}
+}
